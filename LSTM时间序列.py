b1dccfc595304417e6ca4bee3eb31ee5b16ba989
Rhaegal <dapsjj@qq.com>
2021-06-01 17:02:49 +0800

    Update LSTM时间序列.py

--- LSTM时间序列.py
+++ LSTM时间序列.py
@@ -1,11 +1,3 @@
-# https://youtu.be/97bZKO6cJfg
-"""
-Dataset from: https://www.kaggle.com/rakannimer/air-passengers
-International Airline Passengers prediction problem.
-This is a problem where, given a year and a month, the task is to predict 
-the number of international airline passengers in units of 1,000. 
-The data ranges from January 1949 to December 1960, or 12 years, with 144 observations.
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
