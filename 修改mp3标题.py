4cba92dd2e6cbb7d3f116c6426d637b11b7b2c8d
Rhaegal <dapsjj@qq.com>
2022-07-13 20:01:43 +0800

    Update 修改mp3标题.py

--- 修改mp3标题.py
+++ 修改mp3标题.py
@@ -1,5 +1,5 @@
 import eyed3
-audiofile = eyed3.load(r"C:/Users/Administrator/Desktop/audio [vocals].mp3")
+audiofile = eyed3.load(r"C:/Users/Administrator/Desktop/aaa.mp3")
 audiofile.tag.artist = "标题"
 audiofile.tag.title = ""
 audiofile.tag.save()
