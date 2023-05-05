70085cd10ce3c8cf82dc5fd666a83e15f65375bb
Rhaegal <dapsjj@qq.com>
2023-05-05 11:20:26 +0800

    Update labelimg删除xml中的节点.py

--- labelimg删除xml中的节点.py
+++ labelimg删除xml中的节点.py
@@ -1,20 +1,22 @@
-import xml.etree.ElementTree as ET
 import os
+import xml.etree.ElementTree as ET
 
-anno_path = r'C:\Users\aaa\Desktop\test'
+folder_path = "/a/b/c"
 
-xml_list = os.listdir(anno_path)
-xml_list = [ i for i in xml_list if i.endswith('xml')]
-for axml in xml_list:
-    path_xml = os.path.join(anno_path, axml)
-    tree = ET.parse(path_xml)
-    root = tree.getroot()
-    for child in root.findall('object'):
-        xmin = int(child[1][0].text)
-        ymin = int(child[1][1].text)
-        xmax = int(child[1][2].text)
-        ymax = int(child[1][3].text)
-        min_area = (xmax-xmin)*(ymax-ymin)
-        if min_area < 250:
-            root.remove(child)
-    tree.write(os.path.join(anno_path, axml))
+# 遍历文件夹
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith(".xml"):
+            # 打开XML文件并解析
+            xml_path = os.path.join(root, file)
+            tree = ET.parse(xml_path)
+            root = tree.getroot()
+
+            # 找到并删除标签名称为"water"的节点
+            for obj in root.findall('object'):
+                name = obj.find('name').text
+                if name == 'water':
+                    root.remove(obj)
+
+            # 将更新后的XML文件保存
+            tree.write(xml_path)
