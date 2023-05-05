89ba4c701db47886cd85f2c5747ae962cc7820ef
Rhaegal <dapsjj@qq.com>
2023-05-05 11:27:37 +0800

    Update labelimg删除xml中的节点.py

--- labelimg删除xml中的节点.py
+++ labelimg删除xml中的节点.py
@@ -1,7 +1,8 @@
 import os
 import xml.etree.ElementTree as ET
 
-folder_path = "/a/b/c"
+folder_path = r"C:/Users/user1/Desktop/aa"
+label_name= 'water'
 
 # 遍历文件夹
 for root, dirs, files in os.walk(folder_path):
@@ -10,13 +11,13 @@ for root, dirs, files in os.walk(folder_path):
             # 打开XML文件并解析
             xml_path = os.path.join(root, file)
             tree = ET.parse(xml_path)
-            root = tree.getroot()
+            xml_root = tree.getroot()
 
-            # 找到并删除标签名称为"water"的节点
-            for obj in root.findall('object'):
+            # 找到并删除标签名称为label_name的节点
+            for obj in xml_root.findall('object'):
                 name = obj.find('name').text
-                if name == 'water':
-                    root.remove(obj)
+                if name == label_name:
+                    xml_root.remove(obj)
 
             # 将更新后的XML文件保存
             tree.write(xml_path)
