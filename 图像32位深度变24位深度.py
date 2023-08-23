3108fed53c3763ee2bfa51b8b8d6f0d58da562b7
Rhaegal <dapsjj@qq.com>
2023-08-23 14:53:30 +0800

    Update 图像32位深度变24位深度.py

--- 图像32位深度变24位深度.py
+++ 图像32位深度变24位深度.py
@@ -13,12 +13,9 @@ for file_name in file_names:
         
         # 打开图像
         image = Image.open(file_path)
+        # 将图像转换为24位深度
+        image = image.convert("RGB")
         
-        # 检查图像的色彩模式
-        if image.mode == "RGBA":
-            # 将图像转换为24位深度
-            image = image.convert("RGB")
-            
-            # 保存修改后的图像
-            image.save(file_path)
-            print(f"已将 {file_name} 的位深度从32改为24")
+        # 保存修改后的图像
+        image.save(file_path)
+        print(f"已将 {file_name} 的位深度从32改为24")
